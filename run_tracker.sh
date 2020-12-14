091fc7cea14a0706d4802b02c23d4ad6803575f3
derektgardner <dtgardner@gmail.com>
2020-12-13 20:26:18 -0500

    update run_tracker.sh

--- run_tracker.sh
+++ run_tracker.sh
@@ -4,20 +4,20 @@
 # and then loops forever restarting the tracker program if it ever exits.
 #
 
-cd /home/pi/OverPutney
+cd /home/pi/AboveHartland
 source py3/bin/activate
 
 
 while :
 do
 	echo
-	echo '***** Restarting OverPutney ' `date --utc --rfc-3339=ns`
+	echo '***** Restarting AboveHartland ' `date --utc --rfc-3339=ns`
 	echo
 
 	python3 tracker.py
 
 	echo
-	echo '***** OverPutney exited ' `date --utc --rfc-3339=ns`
+	echo '***** AboveHartland exited ' `date --utc --rfc-3339=ns`
 	echo
 
 	sleep 5
